18ded038e352d0bd600aa7923bf9c6b8bba8b004
WhenJavaAttacks <WhenJavaAttacks@users.noreply.github.com>
2017-02-16 13:55:35 -0500

    First Working Version (1.0)

---
+++ tablockdown.js
@@ -0,0 +1,48 @@
+var TabLockdownController = {
+	tabModels: {},
+	init: function() { 
+		console.log("TabLockdownController.init");
+		chrome.browserAction.onClicked.addListener(function(tab) {
+			TabLockdownController.toggleTabLock(TabLockdownController.getTabModelForTab(tab))
+		});
+	},
+	getTabModelForTab: function(tab) {
+		var tabModelKey = "id"+tab.id;
+
+		if (!TabLockdownController.tabModels[tabModelKey]) {
+			TabLockdownController.tabModels[tabModelKey] = {
+				tabId: tab.id,
+				isLocked: false,
+				password: ""
+			};
+		}
+
+		return TabLockdownController.tabModels[tabModelKey];
+	},
+	toggleTabLock: function(tabModel) {
+		if (!tabModel.isLocked) TabLockdownController.lockTab(tabModel);
+		else TabLockdownController.unlockTab(tabModel);
+	},
+	lockTab: function(tabModel) {
+		var enteredPassword = window.prompt("Password");
+		tabModel.password = enteredPassword;
+
+		var codeJS = "document.body.style.visibility = 'hidden';";
+		chrome.tabs.executeScript(tabModel.tabId, { code: codeJS });
+
+		tabModel.isLocked = true;
+		chrome.browserAction.setIcon({ path: "padlock_locked.png", tabId: tabModel.tabId });
+	},
+	unlockTab: function(tabModel) {
+		var enteredPassword = window.prompt("Password");
+		if (!enteredPassword || enteredPassword !== tabModel.password) return;
+
+		var codeJS = "document.body.style.visibility = 'visible';";
+		chrome.tabs.executeScript(tabModel.tabId, { code: codeJS });
+		
+		tabModel.isLocked = false;
+		chrome.browserAction.setIcon({ path: "padlock_unlocked.png", tabId: tabModel.tabId });
+	}
+};
+
+TabLockdownController.init();
